cd80ababcf513dc94b49d4729fdceeffff2c682c
goldshoot0720 <goldshoot0720@gmail.com>
2026-05-25 02:53:20 +0800

    Add Feng Tube channels

--- utils/fengTubeChannels.ts
+++ utils/fengTubeChannels.ts
@@ -58,6 +58,66 @@ export const FENG_TUBE_CHANNELS = [
     label: '喵喵看一看',
     handle: '@%E5%96%B5%E5%96%B5%E7%9C%8B%E4%B8%80%E7%9C%8B',
     url: 'https://www.youtube.com/@%E5%96%B5%E5%96%B5%E7%9C%8B%E4%B8%80%E7%9C%8B/videos'
+  },
+  {
+    id: 'ma-siku',
+    label: '马司库',
+    handle: '@ma-siku',
+    url: 'https://www.youtube.com/@ma-siku/videos'
+  },
+  {
+    id: 'monsterise',
+    label: '怪獸崛起 MONSTERISE',
+    handle: '@monsterise',
+    url: 'https://www.youtube.com/@monsterise/videos'
+  },
+  {
+    id: 'informant510',
+    label: '线人频道Informant',
+    handle: '@informant510',
+    url: 'https://www.youtube.com/@informant510/videos'
+  },
+  {
+    id: 'jilixiaoshimei',
+    label: '吉利小师妹',
+    handle: '@jilixiaoshimei',
+    url: 'https://www.youtube.com/@jilixiaoshimei/videos'
+  },
+  {
+    id: 'sun-channel-hk',
+    label: 'Sun Channel',
+    handle: '@SunChannelHK',
+    url: 'https://www.youtube.com/@SunChannelHK/videos'
+  },
+  {
+    id: 'jlaw',
+    label: '夏河東渡',
+    handle: '@jlaw',
+    url: 'https://www.youtube.com/@jlaw/videos'
+  },
+  {
+    id: 'neixian-zhang',
+    label: '張内咸脫口秀',
+    handle: '@NeixianZhang',
+    url: 'https://www.youtube.com/@NeixianZhang/videos'
+  },
+  {
+    id: 'xiuxianzhe-xiaoye',
+    label: '修仙者小烨',
+    handle: '@%E4%BF%AE%E4%BB%99%E8%80%85%E5%B0%8F%E7%83%A8',
+    url: 'https://www.youtube.com/@%E4%BF%AE%E4%BB%99%E8%80%85%E5%B0%8F%E7%83%A8/videos'
+  },
+  {
+    id: 'xiulianzhe-xiaoye',
+    label: '修炼者小烨',
+    handle: '@xiaoye1757',
+    url: 'https://www.youtube.com/@xiaoye1757/videos'
+  },
+  {
+    id: 'cheapaoe',
+    label: 'cheap',
+    handle: '@cheapaoe',
+    url: 'https://www.youtube.com/@cheapaoe/videos'
   }
 ]
 
